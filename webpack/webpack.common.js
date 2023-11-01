d9aa382e49c0847a0607596a19011b62b6352c57
gptlv <usernamegg@mail.ru>
2023-11-01 11:31:48 +0300

    Add gh-pages

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -35,6 +35,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, "..", "./build"),
     filename: "bundle.js",
+    publicPath: "/webpack-template",
   },
   plugins: [
     new HtmlWebpackPlugin({
