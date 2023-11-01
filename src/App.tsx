37528e3017449ee6ed8dc663acb484f46c4d9b73
gptlv <usernamegg@mail.ru>
2023-11-01 11:43:29 +0300

    Remove unused code

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,7 @@
-import "./styles.css";
-import ClickCounter from "./ClickCounter";
-
 const App = () => {
   return (
     <div>
       {process.env.NODE_ENV} <b>{process.env.name}</b>
-      <ClickCounter />
     </div>
   );
 };
